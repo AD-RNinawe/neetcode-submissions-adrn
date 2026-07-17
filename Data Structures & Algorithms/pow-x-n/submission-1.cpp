11ad24fd6208087e3a97075fd2f97e5d80769eb8
Advait Rajesh Ninawe <four4.fun.99@gmail.com>
2026-07-17 06:50:23 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x==0) return 0;
+        if(n==0) return 1;
+        double res=helper(x,n);
+        return (n>=0) ? res: 1/res;   
+    }
+    double helper(double x, int n){
+        if(n==0) return 1;
+        double half=helper(x,n/2);
+        return (n%2==0) ? half*half: x*half*half;
+    }
+};
